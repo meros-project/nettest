c69a82c5f149aec6c47c6e7bfe63802febbbe985
xoreo <mattnappo@gmail.com>
2020-08-12 19:51:32 -0400

    Writing a custom behavior.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,102 @@
+use async_std::{io, task};
+use futures::prelude::*;
 use libp2p::{
-    core::upgrade, identity::Keypair, secio::SecioConfig, tcp::TcpConfig,
-    yamux, Multiaddr, Transport,
+    build_development_transport, identity,
+    kad::{
+        record::{store::MemoryStore, Key},
+        Kademlia, KademliaEvent, PeerRecord, PutRecordOk, QueryResult,
+        Quorum, Record,
+    },
+    mdns::{Mdns, MdnsEvent},
+    swarm::NetworkBehaviourEventProcess,
+    NetworkBehaviour, PeerId, Swarm,
+};
+use std::{
+    error::Error,
+    task::{Context, Poll},
 };
 
-fn main() {
-    // Before doing anything, create a new keypair for this node's
-    // identity
-    let keypair = Keypair::generate_ed25519();
+fn main() -> Result<(), Box<dyn Error>> {
+    // Create a new key for this peer's identity
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PerrId::from(local_key.public());
 
-    // Create a default tcp transport
-    let base_tcp = TcpConfig::new();
+    // Setup up an encrypted, DNS-enabled TCP transport over
+    // the Mplex protocol.
+    // TODO: Replace this with a manual, stable, upgraded transport
+    let transport = build_development_transport(local_key)?;
 
-    // Upgrade the transport to use secio, for authentication, and
-    // yamux, for multiplexing. But first, create the default configurations
-    // for these protocols.
-    let secio_conf = SecioConfig::new(keypair); // Default secio config
-    let yamux_conf = yamux::Config::default(); // Default yamux config
+    // Create a custom network behavior, combining Kademila and mDNS
+    #[derive(NetworkBehaviour)]
+    struct MyBehavior {
+        kademila: Kademila<MemoryStore>,
+        mdns: Mdns, // TODO: Use bootstrapping here as well (for testing)
+    }
 
-    // Actually upgrade the transport from the base tcp layer
-    let transport = base_tcp
-        .upgrade(upgrade::Version::V1) // Upgrade the network
-        .authenticate(secio_conf) // Negotiate secio as the authentication protocol
-        .multiplex(yamux_conf); // Negotiate yamux as the multiplexing protocol
+    // Start implementing the necessary handlers for `MyBehavior`,
+    // which includes handlers for both mDNS and Kademila
+    impl NetworkBehaviourEventProcess<MdnsEvent> for MyBehavior {
+        // Called when `mdns` (in a MyBehavior instance) produces an event.
+        fn inject_event(&mut self, event: MdnsEvent) {
+            // If the event is a discovery event (that is, if the event
+            // represents (a) peer(s) getting discovered, then do something with
+            // the IDs of the discovered peers). In this case, "something" is
+            // adding the addresses of those peers to the kademila dht (which
+            // is necessary for the dht to work properly).
+            if let MdnsEvent::Discovered(list_of_peers) = event {
+                // for every peer in the list of the peers that were just
+                // discovered, add that peer's identity information to the
+                // kad dht's list of identities.
+                for (peer_id, multiaddr) in list_of_peers {
+                    println!(
+                        "mDNS: discovered peer {:?} {:?}",
+                        &peer_id, &multiaddr
+                    );
+                    self.kademila.add_address(&peer_id, multiaddr);
+                }
+            }
+        }
+    }
 
-    // Parse a multiaddr of the peer to connect to
-    let addr: Multiaddr = "/ip4/192.168.1.208/tcp/20000"
-        .parse()
-        .expect("invalid multiaddr");
+    impl NetworkBehaviourEventProcess<KademilaEvent> for MyBehavior {
+        // Called when `kademila` (in MyBehavior) produces an event.
+        fn inject_event(&mut self, message: KademilaEvent) {
+            // Kademila DHTs have a few different "messages." A message is just
+            // the type of action that is being acted on the dht, such as getting
+            // a record or storing a record. Simply put, its just an event.
+            match message {
+                // If the event is a `QueryResult`, do something.
+                // A `QueryResult` is an event representing when a query to the
+                // dht has produced a result. Check out libp2p::kad::KademilaEvent
+                // for all the variants. Right now, we only care about the QueryResult
+                // event because that is all that this simple dht needs to support:
+                // putting and retrieving records.
+                KademilaEvent::QueryResult { result, .. } => {
+                    // The result here is an enum
+                    // with its own variants representing the types of query results
+                    // that are possible, such as the query being a PUT or a GET.
 
-    // Dial the peer (this returns a future)
-    let _conn = transport.dial(addr).unwrap();
+                    // Match the result,
+                    match result {
+                        // If the query was a record being fetched (and it succeeded),
+                        // do something.
+                        QueryResult::GetRecord(Ok(ok)) => {
+                            for PeerRecord {
+                                record: Record { key, value, .. },
+                                ..
+                            } in ok.records
+                            {
+                                println!(
+                                    "kad dht: Got record {:?} {:?}",
+                                    std::str::from_utf8(key.as_ref())
+                                        .unwrap(),
+                                    std::str::from_utf8(&value).unwrap()
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
